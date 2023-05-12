f3f939a98151fd192b9e7cb0f316273534449422
jennigds <jennifergds89@gmail.com>
2023-05-12 19:30:47 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("   Hora de multiplicar   ");
+
+Console.WriteLine();
+
+Console.Write("Digite o número: ");
+
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Verificar se é multiplo de: ");
+
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % n2 == 0)
+{
+    Console.WriteLine($"\n{n1} é múltiplo de {n2}");
+}
+else
+{
+    Console.WriteLine($"\n{n1} não é múltiplo de {n2}");
+}
+
